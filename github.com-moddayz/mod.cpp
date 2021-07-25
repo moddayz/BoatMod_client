0483af5123b4b4c162e1d26f11655c9621b18a85
moddayz <81351844+moddayz@users.noreply.github.com>
2021-07-25 22:18:46 +0700

    Initial commit

---
+++ github.com-moddayz/mod.cpp
@@ -0,0 +1,6 @@
+name = "Boat SIBNIC Mod"; 				                                          
+tooltip = "Boat MOD by SIBNIC";				                                 
+overview = "This is a collection of copyright mods of the project SIBNIC"; 					
+action = "https://sibnic.info";
+author = "SIBNIC";
+version = "0.1";
